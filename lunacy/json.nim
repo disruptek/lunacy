aa6230e48e0d3b2bb4b15b1138dc0c6646072484
Andy Davidoff <andy+github@disruptek.com>
2020-05-25 18:09:31 -0400

    add json converter

---
+++ lunacy/json.nim
@@ -0,0 +1,32 @@
+import std/json
+
+import lunacy
+
+# converter ...
+converter toJson*(s: LuaStack): JsonNode =
+  if s != nil:
+    s.read
+    case s.kind
+    of TNone, TInvalid, TFunction:
+      discard
+    of TNil:
+      result = newJNull()
+    of TUserData:
+      result = newJString(s.user)
+    of TUserData:
+      result = newJString(s.data)
+    of TThread:
+      result = newJString(s.thread)
+    of TString:
+      result = newJString(s.str)
+    of TNumber:
+      if s.isInteger:
+        result = newJInt(s.integer)
+      else:
+        result = newJNumber(s.num)
+    of TTable:
+      result = newJObject()
+      for key, value in s.tab.pairs:
+        result[$key.toJson] = value.toJson
+    of TBoolean:
+      result = newJBool(s.truthy)
