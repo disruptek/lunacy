f33e7cb0387c21f8639b7e86c967dfcd02d56062
Andy Davidoff <github@andy.disruptek.com>
2020-11-11 13:49:13 -0500

    another attempt to fix repo

--- lunacy/json.nim
+++ lunacy/json.nim
@@ -1,3 +1,4 @@
+import std/tables
 import std/json
 
 import lunacy
@@ -23,10 +24,47 @@ converter toJson*(s: LuaStack): JsonNode =
       if s.isInteger:
         result = newJInt(s.integer)
       else:
-        result = newJNumber(s.num)
+        result = newJFloat(s.num)
     of TTable:
-      result = newJObject()
-      for key, value in s.tab.pairs:
-        result[$key.toJson] = value.toJson
+      block done:
+        let
+          length = len(s.tab)
+
+        if length == 0:
+          result = newJObject()
+          # well, that was easy
+          break done
+
+        # go ahead and create an array of values;
+        # maybe we'll get lucky
+        result = newJArray()
+        for value in values(s.tab):
+          result.add toJson(value)
+
+        var
+          index = 1
+        block array:
+          # check to see if all the indices are ordered
+          # integers; if so, we can just return the values
+          # as a json array
+          for key in keys(s.tab):
+            if key.kind != TNumber or key.integer != index:
+              break array
+            inc index
+          assert index == length + 1  # add one for last inc
+          break done
+
+        # treat it as an object; at least we've already
+        # converted all the values, so we'll reuse them
+        let
+          array = result
+        index = 0
+        result = newJObject()
+        for key in keys(s.tab):
+          # to form an appropriate (string) object key, we
+          # convert the LuaStack to json and stringify it
+          result[$key.toJson] = array.elems[index]
+          inc index
+
     of TBoolean:
       result = newJBool(s.truthy)
