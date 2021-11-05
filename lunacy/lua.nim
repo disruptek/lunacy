ec93a75c377b25d84290af973359c3ca038ab71a
Smooth Operator <github@andy.disruptek.com>
2021-11-05 11:46:24 -0400

    try to help luajit

--- lunacy/lua.nim
+++ lunacy/lua.nim
@@ -42,8 +42,8 @@ when defined(lunacyLuaJIT):
       LibName* = "libluajit.dylib"
   elif defined(UNIX):
     const
-      Name* = "libluajit.so(|.0)"
-      LibName* = "libluajit.so(|.0)"
+      Name* = "libluajit.so(|.2|.0)"
+      LibName* = "libluajit.so(|.2|.0)"
   else:
     const
       Name* = "luajit.dll"
